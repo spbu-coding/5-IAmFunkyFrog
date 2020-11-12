38cf391e3a70a2c4b0cb21b39d91b85934582cba
Stepan <st085873@gmail.com>
2020-11-12 19:31:02 +0300

    modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
-#include <string.h>
 
 void other() {
-    printf("U broken program!!! Congratulations\n");
-    fflush(stdout);
-
-    return;
+	printf("U broken prog");
+	fflush(stdout);
 }
 
 void input() {
-	int a = 0;
-    char buffer[5];
-    memcpy(&a, buffer + 17, 4);
-    printf("%x\n", a);
-    fflush(stdout);
-    scanf("%s", buffer);
-    memcpy(&a, buffer + 17, 4);
-    printf("%x\n", a);
-    puts(buffer);
-    fflush(stdout);
+	char buffer[5];
+	printf("%x%x\n", *((int*)(buffer + 9)), *((int*)(buffer + 13)));
+	gets(buffer);
+	printf("%x%x\n", *((int*)(buffer + 9)), *((int*)(buffer + 13)));
+	puts(buffer);
+	fflush(stdout);
 }
 
-int main() {
-    
-    printf("%p\n", &main);
-    printf("%p\n", &other);
-    input();
-    printf("Normal output\n");
+int main()
+{
+	printf("%p\n", &main);
+	printf("%p\n", &other);
+	input();
+	printf("Normal out");
 
-    return 0;
+	return 0;
 }
-//python -c "print('AAAAAAAAAAAAA\x50\x15\x40\x00')" | main.exe
-//python3 -c "print('AAAAAAAAAAAAA\x04\x12\x00\x80')" | ./main.out
-//python3 -c "print('AAaAaaaaaaaa\x69\x11\x00\x08')" | ./main.out
-//python3 -c "print('AAA')" | ./main.out
-//python3 -c "print('AAaAaaaaaaaaaaaa\xa9\x11\x00\x08')" | ./main.out
+//python3 -c "print('a'*13 + '\x7d\x12\x40\x00')" | ./main.out
+//./main.out < input.txt
